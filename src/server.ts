d5969738db3f994e4bd8f7ede8707776d846052c
Crisler Wintler <crislerwintler@gmail.com>
2021-01-22 21:47:40 -0300

    update tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { request, response } from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Piroca'})
+  return response.json({ message: 'Helloo World'})
 })
 
 app.listen(3000)
