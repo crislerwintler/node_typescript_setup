d05a9a405629f29ad4c9c712b9ff665451c90547
Crisler Wintler <crislerwintler@gmail.com>
2021-01-23 19:05:30 -0300

    Add Typescript paths

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,10 @@
+import { User } from "src/models/User";
+
+
+
+
+export class UserController {
+  teste() {
+    const user = new User();
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { request, response } from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Helloo World'})
+  return response.json({ message: 'y sou um cara muito bonitão'})
 })
 
 app.listen(3000)
